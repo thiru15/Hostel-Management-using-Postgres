2518d229df21ec6761d154e78c42074c915f9de5
thiru15 <33665749+thiru15@users.noreply.github.com>
2019-06-28 17:44:33 +0530

    Add files via upload

---
+++ postgresproject/testapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from testapp.models import Hostel,Student,Course,Room
+
+admin.site.register(Hostel)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Room)
